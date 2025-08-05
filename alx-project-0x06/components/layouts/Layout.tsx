c3c511a5993ac606512d2c628533715f40bb2818
SHEMA IRADUKUNDA Daniel <danieliradukunda00@gmail.com>
2025-08-05 21:24:01 +0200

    wrapping the component in a provider

--- alx-project-0x06/components/layouts/Layout.tsx
+++ alx-project-0x06/components/layouts/Layout.tsx
@@ -1,13 +1,18 @@
 // import { ReactNode } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import { Provider } from "react-redux";
+
 import { LayoutProps } from "@/interface";
+import store from "@/store/store";
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
-      <Header />
-      <main>{children}</main>
-      <Footer />
+      <Provider store={store}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </Provider>
     </>
   );
 };
